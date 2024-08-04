0cb135dce7647dbafd68109a830aee14de3f5c58
JungwooSim <cscd053@gmail.com>
2024-08-04 20:30:45 +0900

    authorization config 추가

---
+++ spring-security-oauth2/oauth2-authorization-server/src/main/java/io/security/oauth2/authorization/AuthorizationServerConfig3.java
@@ -0,0 +1,56 @@
+package io.security.oauth2.authorization;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+@Configuration
+public class AuthorizationServerConfig3 {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer<>();
+    http.apply(authorizationServerConfigurer);
+    return http.build();
+  }
+
+  @Bean
+  public ProviderSettings providerSettings() {
+    return ProviderSettings.builder().issuer("http://Localhost:9000").build();
+  }
+
+  @Bean
+  public RegisteredClientRepository registeredClientRepository() {
+    RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+     .clientId("oauth2-client-app")
+     .clientSecret("{noop}secret")
+     .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+     .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+     .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+     .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+     .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+     .redirectUri("http://127.0.0.1:8081/login/oauth2/code/oauth2-client-app")
+     .redirectUri("http://127.0.0.1:8081")
+     .scope(OidcScopes.OPENID)
+     .scope("message.read")
+     .scope("message.write")
+     .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+     .build();
+
+    return new InMemoryRegisteredClientRepository(registeredClient);
+  }
+
+}
